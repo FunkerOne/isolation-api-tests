21ffe904c5f065dc4e578d39030d0d1c56736f82
FunkerOne <g.maximen@gmail.com>
2026-06-28 17:50:37 +0300

    5.2 HTTP мок-сервис: архитектура и базовая реализация. Практическое задание: HTTP мок для accounts-service

--- tests/mock/http/server.py
+++ tests/mock/http/server.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 from tests.config import test_settings
 from tests.mock.http.api.cards import cards_mock_router
 from tests.mock.http.api.users import users_mock_router
+from tests.mock.http.api.accounts import accounts_mock_router
 
 app = FastAPI(title="mock-service")
 
 app.include_router(users_mock_router)
 app.include_router(cards_mock_router)
+app.include_router(accounts_mock_router)
 
 if __name__ == "__main__":
     uvicorn.run(
